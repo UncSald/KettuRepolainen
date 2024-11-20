4bb977b59ad966913c95804a6bce9c9a12bb46ba
UncSald <iikka.alingue@gmail.com>
2024-11-20 14:20:10 +0200

    Removed paths to todo app.

--- src/app.py
+++ src/app.py
@@ -1,38 +1,14 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
 from entities.todo_repository import get_todos, create_todo, set_done
-from config import app, test_env
+from config import app
 from util import validate_todo
 from entities.reference_creation import create_article_reference
 from entities.reference_repository import get_references
 
-
 @app.route("/")
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished)
-
-@app.route("/new_todo")
-def new():
-    return render_template("new_todo.html")
-
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
-    content = request.form.get("content")
-
-    try:
-        validate_todo(content)
-        create_todo(content)
-        return redirect("/")
-    except Exception as error:
-        flash(str(error))
-        return  redirect("/new_todo")
-
-@app.route("/toggle_todo/<todo_id>", methods=["POST"])
-def toggle_todo(todo_id):
-    set_done(todo_id)
-    return redirect("/")
+    return render_template("index.html")
 
 @app.route("/new_reference")
 def new_reference():
@@ -62,11 +38,3 @@ def references():
         return jsonify(ref), 200   # Return references as JSON with a 200 status code
     except Exception as e:
         return jsonify({"error": str(e)}), 500  # Return an error message if an exception occurs
-
-
-# testausta varten oleva reitti
-if test_env:
-    @app.route("/reset_db")
-    def reset_database():
-        reset_db()
-        return jsonify({ 'message': "db reset" })
