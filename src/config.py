ce5dc72a723aeeca93b3944db0d55c127a51adc7
UncSald <iikka.alingue@gmail.com>
2024-11-20 14:31:51 +0200

    Config.py modified.

--- src/config.py
+++ src/config.py
@@ -3,5 +3,17 @@ from os import getenv
 
 load_dotenv()
 
-SECRET_KEY = getenv("SECRET_KEY")
-DATABASE_URL = getenv("DATABASE_URL")
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+
+database_url = getenv("DATABASE_URL")
+if 'sslmode' not in database_url:
+    if '?' in database_url:
+        database_url += '&sslmode=require'
+    else:
+        database_url += '?sslmode=require'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
