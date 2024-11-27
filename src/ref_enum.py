353abe0c172fdfe548ded45fc420b9c55d8b4a45
UncSald <iikka.alingue@gmail.com>
2024-11-27 19:29:44 +0200

    Added enum for easier tracking.

---
+++ src/ref_enum.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class Reference(Enum):
+    TYPE = 1
+    NAME = 2
+    AUTHOR = 3
+    TITLE = 4
+    JOURNAL = 5
+    YEAR = 6
+    VOLUME = 7
+    NUMBER = 8
+    PAGES = 9
+    MONTH = 10
+    NOTE = 11
+    HOWPUBLISHED = 12
+    EDITOR = 13
+    PUBLISHER = 14
