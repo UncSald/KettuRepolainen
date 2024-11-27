58094a243369ddda8e9f915df0bf98a3159750e6
Matias Palmroth <matias.palmroth@gmail.com>
2024-11-27 10:29:34 +0200

    export button and logic

--- src/app.py
+++ src/app.py
@@ -44,3 +44,64 @@ def create_new_reference():
 def reset_database():
     reset_db()
     return redirect("/")
+
+@app.route("/export_bibtex")
+def export_bibtex():
+    try:
+        references = reference_dao.get_references()
+
+        bibtex_data = ""
+        for ref in references:
+            ref_type = ref.get("type", "misc")
+            if ref_type == "article":
+                bibtex_entry = f"""
+                @article{{{ref["year"]}{ref["name"]},
+                  author = {{{ref["author"]}}},
+                  title = {{{ref["title"]}}},
+                  journal = {{{ref["journal"]}}},
+                  year = {{{ref["year"]}}},
+                  volume = {{{ref["volume"]}}},
+                  number = {{{ref["number"]}}},
+                  pages = {{{ref["pages"]}}},
+                  month = {{{ref["month"]}}},
+                  note = {{{ref["note"]}}}
+                }}
+                """
+            elif ref_type == "book":
+                bibtex_entry = f"""
+                @book{{{ref["year"]}{ref["name"]},
+                  author = {{{ref["author"]}}},
+                  editor = {{{ref["editor"]}}},
+                  title = {{{ref["title"]}}},
+                  publisher = {{{ref["publisher"]}}},
+                  year = {{{ref["year"]}}},
+                  volume = {{{ref["volume"]}}},
+                  number = {{{ref["number"]}}},
+                  pages = {{{ref["pages"]}}},
+                  month = {{{ref["month"]}}},
+                  note = {{{ref["note"]}}}
+                }}
+                """
+            else:
+                bibtex_entry = f"""
+                @misc{{{ref["year"]}{ref["name"]},
+                  author = {{{ref["author"]}}},
+                  title = {{{ref["title"]}}},
+                  howpublished = {{{ref["howpublished"]}}},
+                  year = {{{ref["year"]}}},
+                  month = {{{ref["month"]}}},
+                  note = {{{ref["note"]}}}
+                }}
+                """
+            bibtex_data += bibtex_entry
+
+        return Response(
+            bibtex_data,
+            mimetype="text/plain",
+            headers={"Content-Disposition": "attachment;filename=references.bib",
+		"Content-Type": "application/x-bibtex"
+	    }
+        )
+    except Exception as e:
+        flash(f"Virhe BibTeX-tiedoston luomisessa: {str(e)}", "error")
+        return redirect("/")
