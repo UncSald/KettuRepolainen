490903edc2820c538b28d056adaa6066efe85bf5
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2024-12-04 14:20:21 +0200

    Fixed issue with regex validation for pages field

--- src/app.py
+++ src/app.py
@@ -60,12 +60,13 @@ def create_new_reference():
         errors.append("Keyword already in use!")
 
 
-    reference_pages = request.form["pages"]
+    if request.form.get("pages"):
+        reference_pages = request.form["pages"]
 
-    if reference_pages != None:
-        valid_pages_format = re.compile(r"(\d{1,4}(-[1-9]\d{0,3})?)?")
-        if not re.fullmatch(valid_pages_format, reference_pages):
-            errors.append("Pages need to be written as a number or two numbers divided by a dash (e.g. 1 or 1-25)")
+        if reference_pages != None:
+            valid_pages_format = re.compile(r"(\d{1,4}(-[1-9]\d{0,3})?)?")
+            if not re.fullmatch(valid_pages_format, reference_pages):
+                errors.append("Pages need to be written as a number or two numbers divided by a dash (e.g. 1 or 1-25)")
  
         
     if errors:
